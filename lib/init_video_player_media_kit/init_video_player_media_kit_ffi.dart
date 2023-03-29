54ffa1e781c479a856ee47aab5fd64d4f0805c2d
Abdelaziz Mahdy <zezohassam@gmail.com>
2023-03-29 22:07:33 +0200

    fixed function name

--- lib/init_video_player_media_kit/init_video_player_media_kit_ffi.dart
+++ lib/init_video_player_media_kit/init_video_player_media_kit_ffi.dart
@@ -2,7 +2,7 @@ import 'package:universal_platform/universal_platform.dart';
 import 'package:video_player_media_kit/video_player_media_kit_platform/video_player_media_kit_platform_ffi.dart';
 
 
-void initVideoPlayerMediaKirIfNeeded({bool iosUseMediaKit=false}) {
+void initVideoPlayerMediaKitIfNeeded({bool iosUseMediaKit=false}) {
   if((UniversalPlatform.isWindows||UniversalPlatform.isLinux||UniversalPlatform.isMacOS)&&!UniversalPlatform.isWeb){
     VideoPlayerMediaKit.registerWith();
   }
--- lib/init_video_player_media_kit/init_video_player_media_kit_web.dart
+++ lib/init_video_player_media_kit/init_video_player_media_kit_web.dart
@@ -1,4 +1,4 @@
 
-void initVideoPlayerDartVlcIfNeeded() {
+void initVideoPlayerMediaKitIfNeeded() {
  
 }
