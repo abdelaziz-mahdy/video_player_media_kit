03679bd5dd77b0c0b43a26c0c402ea541a3cc57a
Abdelaziz Mahdy <zezohassam@gmail.com>
2023-03-31 16:48:33 +0200

    added actions

--- test/video_player_media_kit_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:video_player_media_kit/video_player_media_kit.dart';
-
-void main() {
-  test('adds one to input values', () {
-
-  });
-}
